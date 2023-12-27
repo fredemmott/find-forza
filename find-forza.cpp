4d3b69c8d0af974666208d62119074dfb48c0960
Fred Emmott <git@fredemmott.com>
2023-12-27 13:13:43 -0600

    Specifically print out FH5 and FM7

--- find-forza.cpp
+++ find-forza.cpp
@@ -7,6 +7,7 @@
 #include <algorithm>
 #include <format>
 #include <iostream>
+#include <map>
 #include <vector>
 
 using std::cout;
@@ -17,6 +18,17 @@ struct PackageInfo
 	winrt::hstring id;
 	winrt::hstring installedPath;
 };
+
+void PrintPackage(winrt::Windows::ApplicationModel::Package package)
+{
+	// Some displaynames contain unicode characters, like the trademark symbol after 'Thunderbolt'.
+	// Using `wcout` and the wide strings seems to occassionally get confused by this, so this
+	// program is built with the UTF-8 code page forced in the manifest, and uses the UTF-8 conve
+	// 		// program is built with the UTF-8 code page forced in the manifest, and uses the UTF-8 conversions
+	cout << std::format("{}:\n", winrt::to_string(package.Id().FullName()));
+	cout << std::format("\tDisplayName:\t{}\n", winrt::to_string(package.DisplayName()));
+	cout << std::format("\tInstalledPath:\t{}\n", winrt::to_string(package.InstalledPath()));
+}
 
 int main()
 {
@@ -37,18 +49,34 @@ int main()
 
 	for (auto package : packages)
 	{
-		if (package.IsFramework()) {
+		if (package.IsFramework())
+		{
 			continue;
 		}
-		// Some displaynames contain unicode characters, like the trademark symbol after 'Thunderbolt'.
-		// Using `wcout` and the wide strings seems to occassionally get confused by this, so this
-		// program is built with the UTF-8 code page forced in the manifest, and uses the UTF-8 conve
-		// 		// program is built with the UTF-8 code page forced in the manifest, and uses the UTF-8 conversions
-		cout << std::format("{}:\n", winrt::to_string(package.Id().FullName()));
-		cout << std::format("\tDisplayName:\t{}\n", winrt::to_string(package.DisplayName()));
-		cout << std::format("\tInstalledPath:\t{}\n", winrt::to_string(package.InstalledPath()));
+		PrintPackage(package);
 	}
-	cout << std::flush;
 
+	const std::map<const char *, const wchar_t *> wanted{
+		{"Forza Horizon 5", L"Microsoft.624F8B84B80_3.629.845.0_x64__8wekyb3d8bbwe"},
+		{"Forza Motorsport 7", L"Microsoft.ApolloBaseGame_1.174.4791.2_x64__8wekyb3d8bbwe"},
+	};
+
+	for (const auto [label, id] : wanted)
+	{
+		cout << std::format("----------\n{}\n----------\n", label);
+		// found the ID by searching the previous output for "Forza" in the DisplayName
+		auto it = std::ranges::find(packages, id, [](auto pkg)
+									{ return pkg.Id().FullName(); });
+		if (it == packages.end())
+		{
+			cout << "Not found.\n";
+		}
+		else
+		{
+			PrintPackage(*it);
+		}
+	}
+
+	cout << std::flush;
 	return 0;
 }
